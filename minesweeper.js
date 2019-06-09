47522358e81fe2d56bd38b4ef5cd32bfbf7fbeee
Héliton Martins <helitonmrf@gmail.com>
2019-06-09 11:14:51 -0300

    Solved the problem of not identifying the end of the game

--- minesweeper.js
+++ minesweeper.js
@@ -1,13 +1,16 @@
 var field = [];
 var open = [];
 var gameOver = false;
+
 const STATE_COVERED = 0;
 const STATE_OPEN = 1;
 const STATE_FLAGGED = 2;
 const STATE_QUESTION = 3;
-var print_panel_int = null;
+
+var clock_interval = null;
 var flags = 0;
 var num_cells_open = 0;
+
 //Function called by HTML.
 function minesweeper(reff, matrix_width,matrix_height,bombs_num){
     window.ref = reff;
@@ -21,9 +24,10 @@ function minesweeper(reff, matrix_width,matrix_height,bombs_num){
 function init(){
     field = [];
     open = [];
+    num_cells_open = 0;
     gameOver = false;   
-    campo = generate_bombs(matrix_width,matrix_height,bombs_num);
-    matrix = evaluate_fields(campo);
+    var campo = generate_bombs(matrix_width,matrix_height,bombs_num);
+    var matrix = evaluate_fields(campo);
     print(matrix);
     print_panel();
 };
@@ -31,8 +35,8 @@ function init(){
 //Populate the panel with bombs and the clock
 function print_panel(){
     document.getElementById('panel').style.display = 'block';
-    clk = document.getElementById('panel_clock');
-    bmb = document.getElementById('panel_bombs');
+    var clk = document.getElementById('panel_clock');
+    var bmb = document.getElementById('panel_bombs');
     clk.innerHTML = "0:00";
     bmb.innerHTML = window.bombs_num;
     flags = 0;
@@ -40,8 +44,8 @@ function print_panel(){
     start_clock();
 }
 function stop_clock(){
-    clearInterval(print_panel_int);
-    print_panel_int = null;
+    clearInterval(clock_interval);
+    clock_interval = null;
 }
 //write in the panel the number of bombs remaining 
 function print_bombs(){
@@ -49,7 +53,7 @@ function print_bombs(){
 }
 function start_clock(){
     time = 0;
-    print_panel_int = setInterval(function(){
+    clock_interval = setInterval(function(){
         time++;
         min = Math.floor(time/60);
         sec = (time%60 >=10)? time%60 :"0"+time%60 ;
@@ -59,7 +63,7 @@ function start_clock(){
 }
 //Print game according to "open" state.
 function print(matrix){
-    html = "";
+    var html = "";
     for(a = 0; a<matrix.length; a++){
         html+="<tr>";
         for(b = 0; b<matrix[a].length; b++){
@@ -84,13 +88,12 @@ function print(matrix){
 }
 //Write the whole matrix, including the numbers and bombs, independent of state.
 //Called when game overs.
-function print_elements(matrix){//TODO: remove this function
-    html = "";
+function print_elements(matrix){
+    var html = "";
     for(a = 0; a<matrix.length; a++){
         html+="<tr>";
         for(b = 0; b<matrix[a].length; b++){
             if(matrix[a][b]=="*"){
-                // html+="<td class='bomb open' onclick=\"clicked('"+a+","+b+"')\" data-index='"+a+","+b+"'>☼";
                 html+="<td class='bomb open' onclick=\"clicked('"+a+","+b+"')\" data-index='"+a+","+b+"'>☼";
             }else if(matrix[a][b]==0){
                 html+="<td class='empty open' onclick=\"clicked('"+a+","+b+"')\" data-index='"+a+","+b+"'>";
@@ -107,7 +110,7 @@ function print_elements(matrix){//TODO: remove this function
 
 //Init a 0-filled matrix with sizes given and save in window.field.
 function initiate_matrix(matrix_width,matrix_height){
-    campo = [];
+    var campo = [];
     open = [];
     for(a = 0; a<matrix_height; a++){
         campo[a] = [];
@@ -124,10 +127,11 @@ function initiate_matrix(matrix_width,matrix_height){
 //Generate bombs randomly according to "bombs_num" num and matrix size
 function generate_bombs(matrix_width, matrix_height, bombs_num){
     if(matrix_width*matrix_height<bombs_num){
+        console.log("Unable to create more bombs than matrix size.");
         return false;
     }
-    bombs = initiate_matrix(matrix_width,matrix_height);
-    c=0;
+    var bombs = initiate_matrix(matrix_width,matrix_height);
+    var c=0;
     while(c<bombs_num){
         i = Math.floor(Math.random()*(matrix_height));
         j = Math.floor(Math.random()*(matrix_width));
@@ -162,7 +166,7 @@ function clicked(data_index){
     if(gameOver===true){
         return false;
     }
-    indexes = data_index.split(",");
+    var indexes = data_index.split(",");
     var t = reveal(Number(indexes[0]),Number(indexes[1]));
     if(t===false){return false;}
     else if(t==="bomb"){game_over();}
@@ -170,29 +174,29 @@ function clicked(data_index){
         verify_win();
         print(field);
     }
-
     return;
 }
+
 function right_clicked(data_index){
     var indexes = data_index.split(",");
-    i = Number(indexes[0]);
-    j = Number(indexes[1]);
+    var i = Number(indexes[0]);
+    var j = Number(indexes[1]);
     state = open[i][j];
     if(state == STATE_COVERED){
-        $this = document.querySelector("td[data-index='"+i+","+j+"'");
+        var $this = document.querySelector("td[data-index='"+i+","+j+"'");
         $this.classList.add('flagged');
         flags++;
         print_bombs();
         open[i][j] = STATE_FLAGGED;
     }else if(state == STATE_FLAGGED){
-        $this = document.querySelector("td[data-index='"+i+","+j+"'");
+        var $this = document.querySelector("td[data-index='"+i+","+j+"'");
         $this.classList.remove('flagged');
         $this.classList.add('question');
         flags--;
         print_bombs();
         open[i][j] = STATE_QUESTION;
     }else if(state == STATE_QUESTION){
-        $this = document.querySelector("td[data-index='"+i+","+j+"'");
+        var $this = document.querySelector("td[data-index='"+i+","+j+"'");
         $this.classList.remove('question');
         $this.innerHTML="";
         open[i][j] = STATE_COVERED;
@@ -204,27 +208,28 @@ function right_clicked(data_index){
 function reveal(i,j){
     if(open[i][j] == STATE_OPEN || open[i][j] == STATE_FLAGGED || open[i][j] == STATE_QUESTION) return false;
     m = field;
-    c = m[i][j];
-    if(c=="*"){
+    var cell = m[i][j];
+    if(cell=="*"){
         return "bomb";
-    }else if(c!=0){
+    }else if(cell!=0){
         open[i][j]=STATE_OPEN; 
         num_cells_open++;
         return;
     }
     uncover_neighborhood(i,j);
 }
+
 //When empty cell is clicked, its neighborhood are uncovered.
 function uncover_neighborhood(i,j){
-    list = [[i,j]];
+    var list = [[i,j]];
     for(a of list){
-        k = a[0];
-        l = a[1];
-        if(typeof m[k] == "undefined" || typeof m[k][l] == "undefined" || open[k][l]!=STATE_COVERED) continue;
+        var k = a[0];
+        var l = a[1];
+        if(k>=matrix_height || l>= matrix_width || typeof m[k] == "undefined" || typeof m[k][l] == "undefined" || open[k][l]!=STATE_COVERED) continue;
         var d=m[k][l];
         if(d=="*") continue;
-        open[k][l]=STATE_OPEN;   
-        num_cells_open++;     
+        open[k][l]=STATE_OPEN;
+        num_cells_open++;
         if(d!=0) continue;
         list.push([k+1,l+1]);
         list.push([k+1,l-1]);
@@ -236,15 +241,18 @@ function uncover_neighborhood(i,j){
         list.push([k,l+1]);
     }
 }
+
 function verify_win(){
     var cells = matrix_width*matrix_height-bombs_num;
     if(cells==num_cells_open) win();
     else return;
 }
+
 function win(){
     stop_clock();
     alert('Parabéns! Você dominou o tabuleiro com um tempo de '+document.getElementById('panel_clock').innerHTML+'! Jogue novamente para atingir seu melhor tempo, ou escolha um nível mais difícil.');
 }
+
 function game_over(){
     gameOver = true;
     stop_clock();
